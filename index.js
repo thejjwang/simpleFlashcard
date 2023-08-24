8d47acd3783dc12ff9bf7a6b518382806a8fd9c4
thejjwang <jonathanjacobwang@gmail.com>
2023-08-24 15:24:49 -0700

    added references

---
+++ index.js
@@ -0,0 +1,20 @@
+let showAnswerBtn = document.getElementById('showAnswerBtn');
+let correctBtn = document.getElementById('correctBtn');
+let incorrectBtn = document.getElementById('incorrectBtn');
+
+let flashcard = document.getElementById('flashcard');
+
+let scoreValue = document.getElementById('scoreValue');
+
+let myArr = [];
+let currentFlashcardIndex = 0;
+let score = 0 ;
+
+function addFlashcard(question, answer) {
+
+}
+
+function showNextFlashcard() {
+
+
+}
